fa801bd1b630a7f4386c1a433a73c1f0e5501c60
Rodrigo M <rodrigo.medina.neri@gmail.com>
2017-11-03 23:41:38 +0000

    Mapeados los puertos

--- wiringpi_demo.py
+++ wiringpi_demo.py
@@ -13,6 +13,10 @@ wiringpi.pinMode(6, 1)
 
 wiringpi.digitalWrite(6, 1) # enable!
 
+# 7 
+
+secuencia = [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 32, 33, 34, 35, 36, 37, 7, 15, 23, 31]
+
 def binDigitalWrite(num):
     num = bin(num)[2:].zfill(6)
     wiringpi.digitalWrite(0, int(num[5]))
@@ -22,9 +26,10 @@ def binDigitalWrite(num):
     wiringpi.digitalWrite(4, int(num[1]))
     wiringpi.digitalWrite(5, int(num[0]))
 
-binDigitalWrite(0)
+# binDigitalWrite(23)
+# time.sleep(10)
 
-# for i in range(0,39):
-#     binDigitalWrite(i)
-#     time.sleep(5)
+for i in secuencia:
+    binDigitalWrite(i)
+    time.sleep(0.5)
 
